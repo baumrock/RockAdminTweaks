8a83ba51c270dd29a48d159be58bfb9e76848973
BernhardBaumrock <office@baumrock.com>
2024-05-13 09:28:24 +0200

    feat: add quickadd tweak

---
+++ tweaks/PageAdd/QuickAdd.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RockAdminTweaks;
+
+use ProcessWire\HookEvent;
+
+class QuickAdd extends Tweak
+{
+  public function info(): array
+  {
+    return [
+      'description' => 'Skip template selection on page add if only a single template is allowed.',
+    ];
+  }
+
+  public function init(): void
+  {
+    $this->wire->addHookBefore('ProcessPageAdd::buildForm', $this, 'skipAdd');
+  }
+
+  public function skipAdd(HookEvent $event)
+  {
+    $templates = $event->process->getAllowedTemplates();
+    if (count($templates) !== 1) return;
+    foreach ($templates as $k => $tpl) {
+      $p = $this->wire->pages->newPage($tpl);
+      $p->parent = $this->wire->input->get('parent_id', 'int');
+      $p->addStatus('unpublished');
+      $p->save();
+      $this->wire->session->redirect($p->editUrl());
+    }
+  }
+}
