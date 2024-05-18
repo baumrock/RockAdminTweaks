1c327039f22138725c2dfdc9e659525f2f59011b
BernhardBaumrock <office@baumrock.com>
2024-05-18 11:28:43 +0200

    fix: load assets only when needed

--- tweaks/Inputfields/CheckAllCheckboxes/CheckAllCheckboxes.php
+++ tweaks/Inputfields/CheckAllCheckboxes/CheckAllCheckboxes.php
@@ -10,14 +10,13 @@ class CheckAllCheckboxes extends Tweak
     public function info(): array
     {
         return [
-            'description' => "Add checkbox to check all checkboxes in a field",
+            'description' => 'Add checkbox to check all checkboxes in a field',
         ];
     }
 
     public function ready(): void
     {
-        $this->loadCSS();
-        $this->loadJS();
+        $this->wire->addHookAfter('InputfieldCheckboxes::renderReady', $this, 'loadJS');
+        $this->wire->addHookAfter('InputfieldCheckboxes::renderReady', $this, 'loadCSS');
     }
-
 }
